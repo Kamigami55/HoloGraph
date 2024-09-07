2dc03f2ab30833849ee23e958d0b13041d2985d3
Eason Chang <eason@easonchang.com>
2024-09-07 14:00:45 -0600

    Initial Commit

---
+++ HoloGraph/HoloGraphApp.swift
@@ -0,0 +1,33 @@
+//
+//  HoloGraphApp.swift
+//  HoloGraph
+//
+//  Created by Eason on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct HoloGraphApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
---
+++ HoloGraphTests/HoloGraphTests.swift
@@ -0,0 +1,17 @@
+//
+//  HoloGraphTests.swift
+//  HoloGraphTests
+//
+//  Created by Eason on 9/7/24.
+//
+
+import Testing
+@testable import HoloGraph
+
+struct HoloGraphTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
